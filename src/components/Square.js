69af619cbc3edf31cb6919015603c24e0d143268
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-18 22:18:18 +0000

    Create the basic layout of the Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,9 @@ import React from "react";
 class Square extends React.Component {
   render() {
     return (
-
+      <button className="square">
+        {/* TODO */}
+      </button>
     );
   }
 }
