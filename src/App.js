db2b71f594d8881df1e51ce0dff92f22e0c1efbc
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-20 23:20:41 +0000

    Fetch recipes from an API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Recipe from "./components/Recipe";
 
 function App(props) {
+  const APP_ID = '5b82e715';
+  const APP_KEY = '2e4b43adf6d0f870b97747e637895db5';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("meat");
+
+  function getRecipes() {
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      let recipesList = data.hits.map(recipe => {
+        return (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={Math.round(recipe.recipe.calories)}
+            image={recipe.recipe.image}
+          />
+        );
+      });
+      setRecipes(recipesList);
+    });
+  }
+
   return (
     <div>
       <h1>Recipe App</h1>
