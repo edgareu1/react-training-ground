1d5932f443b1ff393097fce5e7027a762aaa218c
Edgar Marques <edgar.eu1@hotmail.com>
2021-02-14 22:25:52 +0000

    Display the products list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <h1>Our products</h1>
+
+        <ProductConsumer>
+          {value => {
+            return value.products.map(product => {
+              return <Product key={product.id} product={product} />;
+            });
+          }}
+        </ProductConsumer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./context";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById("root")
 );
