5b3dd6d8c3c729572b1ff648c8ec318655cdc922
Edgar Marques <edgar.eu1@hotmail.com>
2021-02-15 19:37:24 +0000

    Create the basic layout of the Details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context.js";
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Details</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+
+          return (
+            <div>
+              <h3>{title}</h3>
+
+              <img src={"./images/phone-ecommerce/" + img} alt={title} />
+
+              <div>
+                <h4>Made by:</h4>
+                <p>{company}</p>
+              </div>
+
+              <div>
+                <h4>Price:</h4>
+                <p>{price}</p>
+              </div>
+
+              <p>{info}</p>
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
