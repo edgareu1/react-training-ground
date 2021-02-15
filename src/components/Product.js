dc5eb30f5b3671b99412867263c5179ca00bf7d9
Edgar Marques <edgar.eu1@hotmail.com>
2021-02-15 21:41:22 +0000

    Add the "Add to Cart" button to the Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context.js";
 
 export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
 
     return (
-      <div className="product-card">
-        <h3>{title}</h3>
+      <ProductConsumer>
+        {value => {
+          return (
+            <div className="product-card">
+              <h3>{title}</h3>
 
-        <Link to={"/" + id}>
-          <img src={"./images/phone-ecommerce/" + img} alt={title} />
-        </Link>
-      </div>
+              <Link to={"/" + id}>
+                <img src={"./images/phone-ecommerce/" + img} alt={title} />
+              </Link>
+
+              <button
+                className="cart-btn"
+                disabled={inCart ? true : false}
+                onClick={() => { value.addToCart(id) }}
+              >
+                {inCart ? (
+                  <p>in cart</p>
+                ) : (
+                  <i className="fas fa-cart-plus" />
+                )}
+              </button>
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
