82aade290eae10b30908a4802ca634df63ec4877
Edgar Marques <edgar.eu1@hotmail.com>
2021-02-26 22:47:23 +0000

    Display the increment and decrement CartItem buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context.js";
 
 export default class CartItem extends Component {
   render() {
     const { id, title, img, price, total, count } = this.props.item;
 
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          width: "100%",
-          marginTop: "50px"
+      <ProductConsumer>
+        {value => {
+          const { increment, decrement } = value;
+
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                width: "100%",
+                marginTop: "50px"
+              }}
+            >
+              <img
+                src={"./images/phone-ecommerce/" + img}
+                alt={title}
+                style={{width: "80px", height: "80px"}}
+              />
+              <p>{title}</p>
+              <p>{price}</p>
+              <button onClick={() => { decrement(id) }}>-</button>
+              <p>{count}</p>
+              <button onClick={() => { increment(id) }}>+</button>
+              <p>{total}</p>
+            </div>
+          )
         }}
-      >
-        <img
-          src={"./images/phone-ecommerce/" + img}
-          alt={title}
-          style={{width: "80px", height: "80px"}}
-        />
-        <p>{title}</p>
-        <p>{price}</p>
-        <p>{count}</p>
-        <p>{total}</p>
-      </div>
+      </ProductConsumer>
     );
   }
 }
